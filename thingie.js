7d1c9c9d1e1f7e01a2100d59c0d4906e2e4c1ee9
Haneef <haneef.ghanim@gmail.com>
2016-12-07 22:07:57 -0500

    added/removed some semi-colons

--- thingie.js
+++ thingie.js
@@ -11,83 +11,82 @@ function setup() {
     for(var i=0; i < divs.length; i++) {
         divs[i].style.cursor = 'hand';
 
-		var env = new p5.Env(); //playable sound envelope 
-		env.setADSR(0.001, 0.25, 0.5, 0.5); //time until envelope reaches attack level, 
-											//...... decay level, 
-											//sustain, 
-											//duration of release time 
-		env.setRange(0.5, 0); //attack and release level (volume) when they are complete
-		env.setExp(false); //kinda pointless but can easily replicate the pedal being pushed if false?? idek
+        var env = new p5.Env(); //playable sound envelope
+        env.setADSR(0.001, 0.25, 0.5, 0.5); //time until envelope reaches attack level,
+                                            //...... decay level,
+                                            //sustain,
+                                            //duration of release time
+        env.setRange(0.5, 0); //attack and release level (volume) when they are complete
+        env.setExp(false); //kinda pointless but can easily replicate the pedal being pushed if false?? idek
 
-		var wave = new p5.Oscillator('sine'); // wave going to be played
-		wave.amp(env);
-		wave.start();
-		
-		var divId = divs[i].id; //dict ids
-		wave.freq(dict[divId]); //changed the actual ids to include 4 bc its easier for right now but can change back later
-		envs[divId] = env;			
-		
-		//mouse click on the div
+        var wave = new p5.Oscillator('sine'); // wave going to be played
+        wave.amp(env);
+        wave.start();
+
+        var divId = divs[i].id; //dict ids
+        wave.freq(dict[divId]); //changed the actual ids to include 4 bc its easier for right now but can change back later
+        envs[divId] = env;
+
+        //mouse click on the div
         divs[i].onmousedown = function() {
             keyPressed(this.id);
-			console.log(this.id);
+            console.log(this.id);
         };
-		
+
 
     }
-	
-	//keyboard key is "pressed". Will be used with mouse and computer keyboard
-	function keyPressed(e) {
-		envs[e].triggerAttack();
-	}
-	
-	
-	//Any key is pressed
-	document.onkeypress = function() {
-		myKeyPress(event);
-	}
-	
-	document.onkeyup = function() {
-		keyReleased();
-	}
-	
-	//returns which key was pressed
-	function myKeyPress(e){
-		var keynum;
-		if(window.event) { // IE                    
-		  keynum = e.keyCode;
-		} else if(e.which){ // Netscape/Firefox/Opera                   
-		  keynum = e.which;
-		}
-		
-		var key = String.fromCharCode(keynum);
-		keyNote(key);
-	}
-	
 
-	//Given a key, if it refers to a key on the board, do stuff
-	function keyNote(key){
-		if(key in keys){
-		var note = keys[key];
-			console.log(note);
-			if(note in dict){
-				keyPressed(note);
-				document.getElementById(note).click();
-			}
-		}
-	}
-	
-	
-	// All the release functions independent of the div because they could of moved their mouse away before release
-	//keyboard key is released. Will be used with mouse and computer keyboard
-	function keyReleased() {
-		for(var divId in envs){
-			envs[divId].triggerRelease();
-		}
-	};
-	
-	//mouse release
-	document.onmouseup = function() {
-		keyReleased();
-	};
+    //keyboard key is "pressed". Will be used with mouse and computer keyboard
+    function keyPressed(e) {
+        envs[e].triggerAttack();
+    }
+
+
+    //Any key is pressed
+    document.onkeypress = function() {
+        myKeyPress(event);
+    };
+
+    document.onkeyup = function() {
+        keyReleased();
+    };
+
+    //returns which key was pressed
+    function myKeyPress(e){
+        var keynum;
+        if(window.event) { // IE
+            keynum = e.keyCode;
+        } else if(e.which){ // Netscape/Firefox/Opera
+            keynum = e.which;
+        }
+
+        var key = String.fromCharCode(keynum);
+        keyNote(key);
+    }
+
+
+    //Given a key, if it refers to a key on the board, do stuff
+    function keyNote(key){
+        if(key in keys){
+            var note = keys[key];
+            console.log(note);
+            if(note in dict){
+                keyPressed(note);
+                document.getElementById(note).click();
+            }
+        }
+    }
+
+
+    // All the release functions independent of the div because they could of moved their mouse away before release
+    //keyboard key is released. Will be used with mouse and computer keyboard
+    function keyReleased() {
+        for(var divId in envs){
+            envs[divId].triggerRelease();
+        }
+    }
+    //mouse release
+    document.onmouseup = function() {
+        keyReleased();
+    };
 }
